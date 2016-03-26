9ed8cb098dacc376576feba7586f89181311c681
Sebastian Grobelny <sgrobelny@Sebastians-MacBook-Air-2.local>
2016-03-25 18:49:15 -0700

    Beginner Swift App
    Built static app that calculates one’s pet age from human years to
    animal years.

--- Cat Age/ViewController.swift
+++ Cat Age/ViewController.swift
@@ -10,6 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var CatAge: UITextField!
+    @IBOutlet weak var Age: UITextField!
+   
+    @IBOutlet weak var NewAge: UITextField!
+    
+    @IBAction func Submit(sender: AnyObject) {
+        
+        Age.text = CatAge.text
+        //var str = Age.text
+        //let int1:Int = (Int)(Age.text!)!
+        NewAge.text = (String)(((Int)(Age.text!))!*7)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
